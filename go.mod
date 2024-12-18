42c546ac9b3df02205d4e53eafd48ca5ba3a2961
ISHAAN NENE <ishaannene@gmail.com>
2024-12-18 23:34:59 +0530

    Updates

--- go.mod
+++ go.mod
@@ -4,6 +4,21 @@ go 1.22.0
 
 toolchain go1.22.10
 
-require github.com/gopacket/gopacket v1.3.1
+require (
+	github.com/gopacket/gopacket v1.3.1
+	github.com/jedib0t/go-pretty v4.3.0+incompatible
+)
 
-require golang.org/x/sys v0.24.0 // indirect
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/go-openapi/errors v0.22.0 // indirect
+	github.com/go-openapi/strfmt v0.23.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	go.mongodb.org/mongo-driver v1.14.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,34 +2,80 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"log" 
+	_"os"
 	"github.com/gopacket/gopacket"
 	"github.com/gopacket/gopacket/pcap"
+	"github.com/gopacket/gopacket/layers"
+	_"github.com/jedib0t/go-pretty/table"
+	_"github.com/jedib0t/go-pretty/text"
 )
 
 func main() {
-	handle, err := pcap.OpenLive("lo0", 1600, true, pcap.BlockForever)
+	handle, err := pcap.OpenLive("en0", 1600, true, pcap.BlockForever)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer handle.Close()
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
 	for packet := range packetSource.Packets() {
-		fmt.Println(packet)
+		var srcPort, dstPort uint16
+		var seqNum, ackNum uint32
+		var windowSize uint16
+		var checksum uint16
+		var urgentPointer uint16
+		var protocol string
+
+		if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+			tcp, _ := tcpLayer.(*layers.TCP)
+			srcPort = uint16(tcp.SrcPort)
+			dstPort = uint16(tcp.DstPort)
+			seqNum = tcp.Seq
+			ackNum = tcp.Ack
+			windowSize = tcp.Window
+			checksum = tcp.Checksum
+			urgentPointer = tcp.Urgent
+			protocol = "TCP"
+			fmt.Println(protocol)
+			fmt.Println("+----------------------+-----------------------+")
+			fmt.Printf("| Source Port          | Destination Port      |\n")
+			fmt.Printf("|  %d                  | %d                    |\n", srcPort, dstPort)
+			fmt.Println("+----------------------+-----------------------+")
+			fmt.Printf("| Sequence Number      |\n")
+			fmt.Printf("|         %d            |\n", seqNum)
+			fmt.Println("+---------------------------------------------+")
+			fmt.Printf("| Acknowledgment Number|\n")
+			fmt.Printf("|     %d            |\n", ackNum)
+			fmt.Println("+---+-------+-------+--------------------------+")
+			fmt.Printf("| Data Offset | Reserved | Flags | Window Size  |\n")
+			fmt.Printf("| %d |   0   |   0   |      %d            |\n", 5, windowSize)
+			fmt.Println("+---+-------+-------+--------------------------+")
+			fmt.Printf("| Checksum          | Urgent Pointer          |\n")
+			fmt.Printf("|    %d            | %d                    |\n", checksum, urgentPointer)
+			fmt.Println("+----------------------+-----------------------+")
+			fmt.Println("|             Options (if any, variable)         |")
+			fmt.Println("+---------------------------------------------+")
+			fmt.Println("|                    Data (variable)            |")
+			fmt.Println("+---------------------------------------------+")
+		} else if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+			udp, _ := udpLayer.(*layers.UDP)
+			srcPort = uint16(udp.SrcPort)
+			dstPort = uint16(udp.DstPort)
+			length := udp.Length
+			checksum := udp.Checksum
+			protocol = "UDP"
+			fmt.Println(protocol)
+			fmt.Println("+----------------------+-----------------------+")
+			fmt.Printf("| Source Port          | Destination Port      |\n")
+			fmt.Printf("|  %d                  | %d                    |\n", srcPort, dstPort)
+			fmt.Println("+----------------------+-----------------------+")
+			fmt.Printf("| Length              | Checksum              |\n")
+			fmt.Printf("|       %d            |   %d                  |\n", length, checksum)
+			fmt.Println("+----------------------+-----------------------+")
+			fmt.Println("|                    Data (variable)            |")
+			fmt.Println("+---------------------------------------------+")
+		}
 	}
-    // for packet := range packetSource.Packets() {
-    //     // Network layer
-    //     if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
-    //         ip, _ := ipLayer.(*layers.IPv4)
-    //         fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
-    //     }
-    
-    //     // Transport layer
-    //     if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
-    //         tcp, _ := tcpLayer.(*layers.TCP)
-    //         fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
-    //     }
-    // }
-    
 }
